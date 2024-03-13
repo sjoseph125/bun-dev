eeef215cb09116801162f03c002aab987bb03a00
sjoseph125 <paulsamsonrobert@gmail.com>
2024-03-12 23:01:36 -0400

    added server

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+    port: 5000,
+    fetch(req) {
+        return new Response("Hey Sam");        
+    },
+});
+
+console.log(`Listening on localhost:${server.port}`);
